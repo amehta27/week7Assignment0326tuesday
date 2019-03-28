8b6fa5e802614c96194ef2b394a9516601954259
amehta27 <apmk_27@yahoo.com>
2019-03-28 15:58:20 -0500

    Thursday addon assignment without signout

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const app = express()
+var session = require('express-session')
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -12,17 +18,22 @@ app.set('views','./views')
 app.set('view engine','mustache')
 
 let trips = [
-  {title:"Italy", image:"http:/",date_departure:"03/24/19", date_return:"04/24/19"},
-  {title:"santorini", image:"http:/",date_departure:"04/24/19", date_return:"05/10/19"},
-  {title:"switzerland", image:"http:/",date_departure:"05/10/19", date_return:"05/31/19"}
+  // {title:"Italy", image:"http:/",date_departure:"03/24/19", date_return:"04/24/19"},
+  // {title:"santorini", image:"http:/",date_departure:"04/24/19", date_return:"05/10/19"},
+  // {title:"switzerland", image:"http:/",date_departure:"05/10/19", date_return:"05/31/19"}
 ]
+
+let users = []
 
 app.get('/add-trip', (req, res) => {
   res.render('add-trip.mustache')
 })
 
 app.get('/trips',(req,res) => {
-  res.render('trips',{tripList: trips})
+console.log(req.session.username)
+  let userTrips = trips.filter((trip) => trip.username == req.session.username)
+
+  res.render('trips',{tripList: userTrips, username :req.session.username})
 })
 
 app.post('/add-trip',(req,res) => {
@@ -33,7 +44,8 @@ app.post('/add-trip',(req,res) => {
   let tripDate_return = req.body.tripDate_Departure
 
 
-  let trip = {title: tripTitle, image: tripImage, date_departure: tripDate_Departure, date_return: tripDate_return}
+  let trip = {title: tripTitle, image: tripImage, date_departure: tripDate_Departure, date_return: tripDate_return,
+  username: req.session.username}
   trips.push(trip)
 
   res.redirect('/trips')
@@ -48,12 +60,64 @@ app.post('/trip/delete',(req,res) => {
      return trip.title != title
    })
   res.render('trips',{tripList: trips})
-  
+
 })
   // let user = { name: name, age: age}
   // users.push(user)
   //
   // res.redirect('/users')
+
+  app.get('/login',function(req,res){
+     res.render("login")
+  })
+
+
+
+  app.post('/login',(req,res) => {
+
+    let username = req.body.username
+    let password = req.body.password
+
+
+    let persistedUser = users.find((user) => {
+      return user.userName == username && user.password == password
+    })
+
+    if(persistedUser) {
+      // save username to the session
+      if(req.session){
+          req.session.username = persistedUser.userName
+          console.log(req.session.username)
+          //req.session.age = 12
+          //req.session.user = { username: persistedUser.username, age: 45}
+          res.redirect('/add-trip')
+      }
+
+    } else {
+      //res.redirect('/login')
+      res.render('login',{message: 'Invalid Credentials!!'})
+    }
+
+  })
+
+
+
+  app.get('/register',function(req,res){
+     res.render("register")
+  })
+
+
+
+  app.post("/register", (req,res) =>{
+    let name = req.body.name
+    let userName = req.body.userName
+    let password = req.body.password
+
+    let user = {name:name,userName:userName,password:password}
+    users.push(user)
+    res.redirect("/login")
+  })
+
 
 app.listen(3000,() => {
   console.log("Server is running...")
